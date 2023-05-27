f45eedeb0614a390c60d181650156ac91f32e3ea
knighthinata <nidhi.student.cse22@itbhu.ac.in>
2023-05-27 16:15:14 +0530

    add login.py

---
+++ login.py
@@ -0,0 +1,11 @@
+"""
+As it is always considered a good practice
+to not hard-code your credentials
+
+ID -> Enter your email id with which you are logged in pinterest
+PASSWORD -> your password
+"""
+
+
+ID = ''
+PASSWORD = ''
